003b5e78f586a6938269c4a08a6f3f68342369e0
DanielMbangue <Daniel1mbague@gmail.com>
2026-05-13 21:57:22 -0400

    fic

---
+++ network_diagnostic/pinG_check.py
@@ -0,0 +1,22 @@
+import subprocess
+def check_ping(hostname):
+    result = subprocess.run(
+        ["ping", "-c", "4",hostname],
+        capture_output=True,
+        text =True,
+        timeout=10
+    )
+    if result.returncode != 0:
+        return (False, None)
+    
+    for line in result.stdout.split("\n"):
+        if "min/avg/max" in line:
+            parts = line.split("=")
+            numbers = parts[1].strip().replace(" ms", "")
+            values = numbers.split("/")
+            avg_latency = float(values[1])
+            return (True, avg_latency)
+    return(True, None)
+
+print(check_ping("google.com"))
+print(check_ping("notarealdomain123xyz.com"))
